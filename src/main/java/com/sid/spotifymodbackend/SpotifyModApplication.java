1bd865bc3a0768a37938cec81c5a62d337bfffe8
siddheswarojha <siddheswarojha23@gmail.com>
2022-05-02 10:11:35 +0530

    Initializing Empty Backend Repo

---
+++ src/main/java/com/sid/spotifymodbackend/SpotifyModApplication.java
@@ -0,0 +1,13 @@
+package com.sid.spotifymodbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyModApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyModApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sid/spotifymodbackend/SpotifyModApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.spotifymodbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyModApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
